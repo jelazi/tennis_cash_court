30779535e0a0485b2ea92844697372eafdf8ab49
jelazi <lzizka@gmail.com>
2022-01-27 20:48:32 +0100

    WIP: firebase set get

--- lib/model/tennis_hour.dart
+++ lib/model/tennis_hour.dart
@@ -1,8 +1,8 @@
 class TennisHour {
-  double hours;
-  DateTime date;
-  String partner;
-  bool _isSold = false;
+  late double hours;
+  late DateTime date;
+  late String partner;
+  late bool _isSold = false;
 
   bool get isSold {
     return _isSold;
@@ -12,5 +12,22 @@ class TennisHour {
     _isSold = isSold;
   }
 
+  Map toMap() {
+    Map map = {
+      'hours': hours,
+      'date': date.toString(),
+      'partner': partner,
+      'isSold': _isSold.toString()
+    };
+    return map;
+  }
+
   TennisHour(this.date, this.hours, this.partner);
+
+  TennisHour.fromMap(Map map) {
+    date = (map['date'] != null) ? DateTime.parse(map['date']) : DateTime.now();
+    hours = (map['hours'] != null) ? double.parse(map['hours'].toString()) : 0;
+    partner = map['partner'] ?? '';
+    _isSold = map['isSold'] == 'true' ? true : false;
+  }
 }

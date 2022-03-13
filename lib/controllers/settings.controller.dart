41ba30931f8a7b84ab6d5625e8fd693dc85dbb89
jelazi <lzizka@gmail.com>
2022-03-13 16:32:32 +0100

    WIP: player update

--- lib/controllers/settings.controller.dart
+++ lib/controllers/settings.controller.dart
@@ -1,9 +1,12 @@
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
+import '../model/player.dart';
+
 class SettingsController extends GetxController {
   RxInt priceForHour = RxInt(100);
   RxString currency = RxString('Kč');
+  Player? currentPlayer;
 
   SettingsController() {
     loadData();
@@ -13,11 +16,13 @@ class SettingsController extends GetxController {
     final box = GetStorage();
     priceForHour = RxInt(await box.read('priceForHour') ?? 100);
     currency = RxString(await box.read('currency') ?? 'Kč');
+    currentPlayer = Player.fromMap(await box.read('current_player'));
   }
 
   saveData() async {
     final box = GetStorage();
     await box.write('priceForHour', priceForHour.value);
     await box.write('currency', currency.value);
+    await box.write('current_player', currentPlayer?.toMap());
   }
 }
